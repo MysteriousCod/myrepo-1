c344cfad8c140497536a51ea4280c1b57fc08f7b
MysteriousCod <urkuderam9049@gmail.com>
2025-05-18 15:38:31 +0530

    my first b1 commit

--- hello.py
+++ hello.py
@@ -1,20 +1,21 @@
 # addition.py
 
-def add_numbers(num1, num2, num3):
+def add_numbers(num1, num2, num3, num4):
     """Function to add two numbers."""
-    return num1 + num2 + num3
+    return num1 + num2 + num3 + num4
 
 def main():
     # Ask for user input
     try:
         number1 = int(input("Enter the first number: "))
         number2 = int(input("Enter the second number: "))
-        number3 = int(input("Enter the third number: ")) 
+        number3 = int(input("Enter the third number: "))
+        number4 = int(input("Enter the fourth number: "))
         # Add the numbers
-        result = add_numbers(number1, number2, number3)
+        result = add_numbers(number1, number2, number3, number4)
         
         # Print the result
-        print(f"The sum of {number1} and {number2} and {number3} is: {result}")
+        print(f"The sum of {number1} and {number2} and {number3} and {number4} is: {result}")
     except ValueError:
         print("Please enter valid numbers.")
 
